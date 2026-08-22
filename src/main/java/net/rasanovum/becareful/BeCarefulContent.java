4aac34b078ef77b57c435d27808cbf729eeab281
xameryn <maximilianramann@gmail.com>
2026-08-22 12:14:12 -0700

    Updated textures

--- src/main/java/net/rasanovum/becareful/BeCarefulContent.java
+++ src/main/java/net/rasanovum/becareful/BeCarefulContent.java
@@ -18,16 +18,11 @@ import net.rasanovum.rosetta.registry.RegistryHandle;
 public final class BeCarefulContent {
     public static final ModRegistrar REGISTRAR = new ModRegistrar(BeCareful.MOD_ID);
 
-    public static final RegistryHandle<net.minecraft.world.effect.MobEffect> CORRUPTION =
-            REGISTRAR.register(BuiltInRegistries.MOB_EFFECT, "corruption", CorruptionEffect::new);
-    public static final RegistryHandle<Item> TOTEM_OF_LIGHT =
-            REGISTRAR.item("totem_of_light", TotemOfLight::new, new Item.Properties().stacksTo(1));
-    public static final RegistryHandle<Item> ECHO_SHARD_DUST =
-            REGISTRAR.item("echo_shard_dust", Item::new, new Item.Properties());
-    public static final RegistryHandle<Item> LOST_KEY =
-            REGISTRAR.item("lost_key", Item::new, new Item.Properties().stacksTo(1).rarity(Rarity.EPIC));
-    public static final RegistryHandle<Item> FROZEN_CORE =
-            REGISTRAR.item("frozen_core", Item::new, new Item.Properties().stacksTo(16).rarity(Rarity.UNCOMMON));
+    public static final RegistryHandle<net.minecraft.world.effect.MobEffect> CORRUPTION = REGISTRAR.register(BuiltInRegistries.MOB_EFFECT, "corruption", CorruptionEffect::new);
+    public static final RegistryHandle<Item> TOTEM_OF_LIGHT = REGISTRAR.item("totem_of_light", TotemOfLight::new, new Item.Properties().stacksTo(1));
+    public static final RegistryHandle<Item> ECHO_SHARD_DUST = REGISTRAR.item("echo_shard_dust", Item::new, new Item.Properties());
+    public static final RegistryHandle<Item> LOST_KEY = REGISTRAR.item("lost_key", Item::new, new Item.Properties().stacksTo(1).rarity(Rarity.EPIC));
+    public static final RegistryHandle<Item> FROZEN_CORE = REGISTRAR.item("frozen_core", Item::new, new Item.Properties().stacksTo(16).rarity(Rarity.UNCOMMON));
 
     public static final ModRegistrar.BlockItemEntry<FrozenCampfireBlock, BlockItem> FROZEN_CAMPFIRE =
             REGISTRAR.blockWithItem(
@@ -44,8 +39,7 @@ public final class BeCarefulContent {
                     new Item.Properties().stacksTo(16).rarity(Rarity.RARE)
             );
 
-    public static final RegistryHandle<BlockEntityType<FrozenCampfireBlockEntity>> FROZEN_CAMPFIRE_ENTITY =
-            REGISTRAR.blockEntity("frozen_campfire_be", FrozenCampfireBlockEntity::new, FROZEN_CAMPFIRE.block());
+    public static final RegistryHandle<BlockEntityType<FrozenCampfireBlockEntity>> FROZEN_CAMPFIRE_ENTITY = REGISTRAR.blockEntity("frozen_campfire_be", FrozenCampfireBlockEntity::new, FROZEN_CAMPFIRE.block());
 
     private BeCarefulContent() {}
 }
